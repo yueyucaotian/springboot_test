4058bb0495ed87a23fe8ea9a7349670bae7e6673
曹添 <1614330634@qq.com>
2019-10-18 16:20:04 +0800

    更改类set和get方式

--- src/main/java/com/example/demo/pojo/User.java
+++ src/main/java/com/example/demo/pojo/User.java
@@ -1,10 +1,14 @@
 package com.example.demo.pojo;
 
-import org.beetl.sql.core.annotatoin.AutoID;
+
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
-import java.util.Date;
 
+
+@Setter
+@Getter
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -43,139 +47,4 @@ public class User implements Serializable {
 
     private String token;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public int getLockVersion() {
-        return lockVersion;
-    }
-
-    public void setLockVersion(int lockVersion) {
-        this.lockVersion = lockVersion;
-    }
-
-    public String getLoginName() {
-        return loginName;
-    }
-
-    public void setLoginName(String loginName) {
-        this.loginName = loginName;
-    }
-
-    public String getLoginEmail() {
-        return loginEmail;
-    }
-
-    public void setLoginEmail(String loginEmail) {
-        this.loginEmail = loginEmail;
-    }
-
-    public String getLoginPhone() {
-        return loginPhone;
-    }
-
-    public void setLoginPhone(String loginPhone) {
-        this.loginPhone = loginPhone;
-    }
-
-    public String getNickName() {
-        return nickName;
-    }
-
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
-    }
-
-    public String getPwd() {
-        return pwd;
-    }
-
-    public void setPwd(String pwd) {
-        this.pwd = pwd;
-    }
-
-    public long getCreatePerson() {
-        return createPerson;
-    }
-
-    public void setCreatePerson(long createPerson) {
-        this.createPerson = createPerson;
-    }
-
-    public String getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(String createTime) {
-        this.createTime = createTime;
-    }
-
-    public String getEnableState() {
-        return enableState;
-    }
-
-    public void setEnableState(String enableState) {
-        this.enableState = enableState;
-    }
-
-    public String getaType() {
-        return aType;
-    }
-
-    public void setaType(String aType) {
-        this.aType = aType;
-    }
-
-    public String getLastLoginTime() {
-        return lastLoginTime;
-    }
-
-    public void setLastLoginTime(String lastLoginTime) {
-        this.lastLoginTime = lastLoginTime;
-    }
-
-    public String getLastLoginIp() {
-        return lastLoginIp;
-    }
-
-    public void setLastLoginIp(String lastLoginIp) {
-        this.lastLoginIp = lastLoginIp;
-    }
-
-    public String getOrderby() {
-        return orderby;
-    }
-
-    public void setOrderby(String orderby) {
-        this.orderby = orderby;
-    }
-
-    public String getSingleLogin() {
-        return singleLogin;
-    }
-
-    public void setSingleLogin(String singleLogin) {
-        this.singleLogin = singleLogin;
-    }
-
-    public String getActivationTime() {
-        return activationTime;
-    }
-
-    public void setActivationTime(String activationTime) {
-        this.activationTime = activationTime;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
 }
